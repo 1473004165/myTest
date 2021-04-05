75e32a421c5907717ff7d488696f924e74e52aac
ZhangBu <1659456850@qq.com>
2021-04-05 19:10:32 +0800

    查询管理员信息接口、修改接口层级目录

--- src/main/java/com/dongye/sanquan/api/pc/adminUser/controller/AdminUserController.java
+++ src/main/java/com/dongye/sanquan/api/pc/adminUser/controller/AdminUserController.java
@@ -4,13 +4,11 @@ package com.dongye.sanquan.api.pc.adminUser.controller;
 import com.dongye.sanquan.api.pc.adminUser.service.AdminUserService;
 import com.dongye.sanquan.pojo.orm.SysAdminEntity;
 import com.dongye.sanquan.pojo.rmso.ResultVO;
+import com.dongye.sanquan.utils.ShiroUtils;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -23,22 +21,29 @@ import javax.annotation.Resource;
  * @since 2021-04-05
  */
 @RestController
-@RequestMapping("/adminUser")
+@RequestMapping("/pc/admin")
 public class AdminUserController {
 
     @Resource
     AdminUserService adminUserService;
 
     /**
-     * 注册用户
+     * 注册管理员
      *
      * @return int
      * @throws Exception e
      */
-    @PostMapping("/addUser")
+    @PostMapping("/adminAdd")
     @ApiOperation("注册")
     public ResultVO addDate(@RequestBody SysAdminEntity sysAdminEntity) throws Exception {
         return adminUserService.addData(sysAdminEntity);
+    }
+
+    @GetMapping("/adminInfo")
+    @ApiOperation("查询管理员信息")
+    public ResultVO<SysAdminEntity> adminInfo(){
+        Long userId = ShiroUtils.getAdminInfo().getAdminId();
+        return new ResultVO<SysAdminEntity>(adminUserService.getById(userId));
     }
 
 }
--- src/main/java/com/dongye/sanquan/config/CorsConfig.java
+++ src/main/java/com/dongye/sanquan/config/CorsConfig.java
@@ -7,21 +7,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-/**跨域配置
+/**
+ * 跨域配置
+ *
  * @author 章卜
- * @version 1.0
- * @date 2021/04/04 0:16
+ * @since 2020-12-16
  */
 @Configuration
 public class CorsConfig {
 
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // 1允许任何域名使用
         corsConfiguration.addAllowedOrigin("*");
-        // 2允许任何头
         corsConfiguration.addAllowedHeader("*");
-        // 3允许任何方法（post、get等）
         corsConfiguration.addAllowedMethod("*");
         return corsConfiguration;
     }
@@ -30,7 +28,7 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", buildConfig());
-        return new CorsFilter(source);
+        return new CorsFilter();
     }
 
 }
